5a0e2009e912df7ddc0b8aa61e8b128e8831aac2
Jennifer Brito <jennifer.brito3@gmail.com>
2021-05-12 21:11:30 -0300

    tela de login adicionada

---
+++ lib/providers/cadastro_user.dart
@@ -0,0 +1,110 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_signin_button/button_builder.dart';
+import 'package:flutter/material.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class Cadastro extends StatefulWidget {
+  final String title = 'Cadastro';
+  @override
+  _CadastroState createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _success;
+  String _userEmail = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Form(
+          key: _formKey,
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(labelText: 'Email'),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Digite email';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(labelText: 'Senha'),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Digite uma senha';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      alignment: Alignment.center,
+                      child: SignInButtonBuilder(
+                        icon: Icons.person_add,
+                        backgroundColor: Colors.blueGrey,
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            await _register();
+                          }
+                        },
+                        text: 'Cadastrar',
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: Text(_success == null
+                          ? ''
+                          : (_success
+                              ? 'Cadastro realizado com sucesso $_userEmail'
+                              : 'Falha no cadastro')),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  // Example code for registration.
+  Future<void> _register() async {
+    final User user = (await _auth.createUserWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    ))
+        .user;
+    if (user != null) {
+      setState(() {
+        _success = true;
+        _userEmail = user.email;
+      });
+    } else {
+      _success = false;
+    }
+  }
+}
